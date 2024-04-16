7df59108ed8da541de76031ea4b2dd1a64fcb1f5
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-16 13:13:35 +0100

    creates mysql config file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,11 @@
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE DATABASE IF NOT EXISTS performance_schema;
 
-USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'%';
-FLUSH PRIVILEGES;
+-- Create user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-USE performance_schema;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'%';
-FLUSH PRIVILEGES;
+-- Grant privileges to the user on the database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
