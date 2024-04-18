8823251f95ab49bb85ee5832e003f1ab1ecf6a58
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-18 13:58:58 +0100

    fixes pycodesyle flags in file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,11 +24,8 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
- #   def delete(self, obj=None):
-#        if obj :
-            
-            
-            
+            #   def delete(self, obj=None):
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -38,7 +35,6 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -49,6 +45,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
